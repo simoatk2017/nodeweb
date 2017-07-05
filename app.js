b0b4ebfb5d87faced726a5e2dcb244f25093112c
simoatk2017 <ggr_fi@yahoo.com>
2017-07-05 17:54:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 var mysql      = require('mysql');
-var things = require('./routes/customers.js');
+var customers = require('./routes/customers.js');
 
-app.use(things);
+app.use(customers);
 app.use(errorHandler);
 
 // error handler middleware
